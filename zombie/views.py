ecb91ee7d6ce1706af5a33394272c54279ab119c
Nick Moore <nick@zoic.org>
2018-07-27 08:47:21 +1000

    remove old handler method moved to handlers.py

--- zombie/views.py
+++ zombie/views.py
@@ -4,34 +4,6 @@ Zombie pages and their behaviours.
 
 class View:
 
-    # XXX this should probably be a separate zombie.bottle.handler 
-    # XXX this is very simple and doesn't allow for multiple BFF servers: a mechanism
-    #     to pin a session to a BFF server would be nice.
-
-    @classmethod
-    def bottle_handler(cls):
-        import bottle
-        global sessions
-        if bottle.request.method == 'GET':
-            session_id = secrets.token_hex(16)
-            sessions[session_id] = cls()
-            return loader_html % (loader_js(bottle.request.path, session_id))
-        elif bottle.request.method == 'POST':
-            session_id = bottle.request.params.get('s')
-            try:
-                view_obj = sessions[session_id]
-            except KeyError:
-                # Reset browser state by reloading page
-                return "alert('Session Expired'); document.location = " + repr(bottle.request.path)
-
-            return view_obj.event(
-                number = bottle.request.params.get('n', 0),
-                value = bottle.request.params.get('v')
-            )
-        else:
-            bottle.response = 405
-            return "Method Not Allowed"
-
     def __init__(self):
         self.events = [ self.load ]
 
